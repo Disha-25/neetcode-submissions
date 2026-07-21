a90fe71cbbb5a226712fa67c12a29a6e393f8202
Disha-25 <93224436+Disha-25@users.noreply.github.com>
2026-07-22 00:59:28 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        # ord is used to get unique code value of any character
+        score = 0
+        for i in range(len(s)-1):
+            score += abs(ord(s[i+1]) - ord(s[i]))
+        return score
+        
