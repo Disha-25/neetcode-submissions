e14c653e5b1da763fd655faa09b518f4b3f99b16
Disha-25 <93224436+Disha-25@users.noreply.github.com>
2026-07-27 00:23:15 +0530

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # O(n*k), O(1)
+        # i = 0
+        # while i < k:
+        #     temp = nums[-1]
+        #     nums.pop()
+        #     nums.insert(0, temp) # insert - O(n) linear-time operation
+        #     i+=1
+        
+
+        n = len(nums)
+        k %= n
+
+        def reverse(l: int, r: int) -> None:
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l + 1, r - 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
